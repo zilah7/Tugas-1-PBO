275263ac60dc456a1522d866b36df7657bbb402e
Nazilah <163401104+zilah7@users.noreply.github.com>
2025-03-30 20:27:48 +0700

    -
    -

---
+++ CustomOutput.java
@@ -0,0 +1,8 @@
+public class CustomOutput {
+    public static void main(String[] args) {
+        // Menampilkan output sesuai dengan instruksi
+        System.out.println("Ini adalah program JAVA yang lebih kompleks.");
+        System.out.println("Mari kita belajar lebih dalam tentang pemrograman.");
+        System.out.println("Dengan JAVA, kita bisa membuat aplikasi yang hebat!");
+    }
+}
---
+++ PrintOut.java
@@ -0,0 +1,7 @@
+public class PrintOutput {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
